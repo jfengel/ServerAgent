a05cfd16c3787cc332e613d6c5276b4c15f7a1bd
Joshua Engel <engel@highfleet.com>
2017-10-11 13:22:30 -0400

    Completing the mavenizing rearrangement

---
+++ src/main/java/net/purgo/serverAgent/Data.java
@@ -0,0 +1,80 @@
+package net.purgo.serverAgent;
+
+import java.util.*;
+
+/** Data being collected by the server agent */
+public class Data {
+    // TODO this really ought to become a circular queue
+    public static List<String> responses = Collections.synchronizedList(new ArrayList<>());
+
+    static class IntThreadLocal extends ThreadLocal<int[]>{
+        @Override
+        public int[] initialValue() {
+            return new int[] { -1 };
+        }
+    }
+
+    static class LongThreadLocal extends ThreadLocal<long[]>{
+        @Override
+        public long[] initialValue() {
+            return new long[1];
+        }
+
+        public void set(long l) {
+            get()[0] = l;
+        }
+        public long value() {
+            return get()[0];
+        }
+
+    }
+
+
+    /** Counts each time call is called */
+    static IntThreadLocal counter = new IntThreadLocal();
+
+    /** Reset each time begin is called. */
+    static IntThreadLocal startCount = new IntThreadLocal();
+
+    static LongThreadLocal lastMemoryMark = new LongThreadLocal();
+
+
+    static LongThreadLocal startTime = new LongThreadLocal();
+
+    /** Increments the per-thread string count.
+     */
+    public static void call() {
+        counter.get()[0]++;
+    }
+
+
+    /** Increments the per-thread string count.
+     * @param s -- currently ignored
+     */
+    public static void call(String s) {
+        counter.get()[0]++;
+    }
+
+    /** Call this to begin a counting session */
+    public static void begin(String source) {
+        int start = startCount.get()[0];
+        if(start < 0) {
+            startCount.get()[0] = counter.get()[0];
+            startTime.set(System.currentTimeMillis());
+            lastMemoryMark.set(Runtime.getRuntime().freeMemory());
+        }
+    }
+
+    /** Call this to end a counting session */
+    public static void end(String source) {
+        int start = startCount.get()[0];
+        if(start < 0)
+            return;
+        int total = counter.get()[0] - start;
+        long time = (System.currentTimeMillis() - startTime.value());
+        long mem = lastMemoryMark.value() - Runtime.getRuntime().freeMemory();
+        responses.add(source + ": " + total + " strings in " +
+                time + "ms with memory " + mem + " from thread " + Thread.currentThread() );
+        startCount.get()[0] = -1;
+    }
+}
